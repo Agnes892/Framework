8cb0d975843980575ab2c000bff0baf2e1a9fa2e
Agnes892 <agnesdamotaveloso@gmail.com>
2025-07-01 08:37:03 -0400

    atualizacoes

--- app/libraries/controles.php
+++ app/libraries/controles.php
@@ -1,18 +1,16 @@
 <?php
 class Controllers{
     public function model($model){
-        require_once  '../app/Models/'.$model.'.php';
+        require_once '../app/Models/'.$model.'.php';
         return new $model;
-    }
+    }//fim da funcão model
 
     public function view($view, $dados = []){
-        $arquivo = ('../app/View/').$view.'.php';
+        $arquivo = ('../app/Views/'.$view.'.php');
         if(file_exists($arquivo)){
             require_once $arquivo;
-        }
-        else{
+        }else{
             die("O arquivo não existe");
-        }
-    }
-}
-?>
+        }// fim do if
+    }// fim do método view
+}//fim da classe Controllers
--- public/index.php
+++ public/index.php
@@ -1,18 +1,18 @@
 <?php
-include '../app/libraries/Rota.php';
+include '../app/Libraries/Rota.php';
+include '../app/Libraries/Controles.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FRAMEWORK</title>
+    <title>Framework</title>
 </head>
 <body>
-    <?php
-    $rotas = new Rota();
-    $rotas->url();
-    ?>
+   <?php
+   $rotas = new Rota();
+   //$rotas->url();
+   ?>
 </body>
 </html>
